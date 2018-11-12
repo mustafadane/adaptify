1f38046759ea32ad864f6f5c74a84cd428847e02
Mustafa Dane <mustafadane@gmail.com>
2018-11-11 19:35:27 -0600

    plays a playlist and skips a song

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Alert, TouchableHighlight, Button} from 'react-native';
 import Spotify from 'rn-spotify-sdk'
 
 const instructions = Platform.select({
@@ -19,12 +19,97 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(){
+		super();
+		  this.state = {
+        spotifyInitialized: false,
+        loggedIn: false,
+        profile: {},
+        playlists: []
+      };
+		  this.handleLogin = this.handleLogin.bind(this);
+	  }
+
+
+    async componentDidMount(){
+      if(!Spotify.isInitialized()){
+        let spotifyOptions = {
+          "clientID": "178d7b96e8be4134ab1747a83f21ef54",
+          "sessionUserDefaultsKey":"SpotifySession",
+          "redirectURL":"https://adaptify.herokuapp.com/callback",
+          "scopes":["user-read-private", "playlist-read", "playlist-read-private", "streaming"],
+          "tokenSwapURL":"https://adaptify.herokuapp.com/swap",
+          "tokenRefreshURL":"https://adaptify.herokuapp.com/refresh",
+          "android": {
+            "loginLoadingText": "Are you ready for some cool stuff?"
+          }
+
+        }
+        try{
+          const loggedIn = await Spotify.initialize(spotifyOptions)
+          this.setState({spotifyInitialized: true})
+          if(loggedIn){
+
+            this.setState({loggedIn: true})
+
+          }
+        } catch (err) {
+          Alert.alert("Error", err.message)
+        }
+
+      }
+    }
+
+    async handleLogin(){
+      const loggedIn = await Spotify.login()
+      try {
+        if(loggedIn){
+          const profile = await Spotify.getMe()
+          // const playlists = await Spotify.sendRequest("/v1/me/playlists", "GET", {limit:1, offset:0}, true)
+          this.setState({loggedIn: true, profile})
+        }
+      } catch (err){
+        Alert.alert("Error", err.message)
+      }
+    }
+    handleLogout = async () => {
+      await Spotify.logout()
+      this.setState({loggedIn: false})
+    }
+    handlePlay = async () => {
+      await Spotify.playURI("spotify:user:dbm5upwtjf00xribeoglqqaw6:playlist:2H5nOGw4kwD1kAHliRAtR4", 0, 0)
+
+    }
+    handleSkip = async () => {
+      await Spotify.skipToNext( )
+      // Alert.alert(await Object.values(Spotify.getPlaybackMetadata()).toString())
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native!!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+
+        <Text style={styles.instructions}>Is initialized: {this.state.spotifyInitialized.toString()}</Text>
+        <Text style={styles.instructions}>Is loggedIn: {this.state.loggedIn.toString()}</Text>
+        <Text style={styles.instructions}>Me: {Object.keys(this.state.profile)}</Text>
+        <Text style={styles.instructions}>Me: {this.state.profile.display_name}</Text>
+        <Text style={styles.instructions}>Playlists: {this.state.playlists}</Text>
+        <TouchableHighlight onPress={this.handleLogin}>
+						<Text>Log into Spotify</Text>
+					</TouchableHighlight>
+        <TouchableHighlight onPress={this.handleLogout}>
+						<Text>Log out</Text>
+					</TouchableHighlight>
+        <Button onPress={this.handleSkip} title='Skip' />
+        <Button onPress={this.handlePlay} title='Play' />
+        {/* <Button onPress={async ()=> {
+          await Spotify.setPlaying({playing: true})}}
+          title='Play' /> */}
+
       </View>
     );
   }
